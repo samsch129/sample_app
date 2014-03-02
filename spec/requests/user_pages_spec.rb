e984003acb5ea41c4b308ae91aea1bef2dd81c07
Sam Schultz <samuelcschultz@gmail.com>
2014-03-01 16:16:14 -0800

    Finish layout and routes

---
+++ spec/requests/user_pages_spec.rb
@@ -0,0 +1,21 @@
+def full_title(page_title)
+  base_title = "Ruby on Rails Tutorial Sample App"
+  if page_title.empty?
+	base_title
+  else
+    "#{base_title} | #{page_title}"
+  end
+end
+
+require 'spec_helper'
+
+describe "User pages" do
+  subject { page }
+  
+  describe "signup page" do
+    before { visit signup_path }
+
+    it { should have_selector('h1', text: "Sign up")}
+    it { should have_selector('title', text: full_title("Sign up")) }
+  end 
+end
